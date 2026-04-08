7f71448bcfae418d2fa096f4b9a850ad3b94e4fd
Jack Kelly <jack@openclimatefix.org>
2026-04-08 10:27:30 +0100

    Try new plugin to catch Gemma errors. Doesn't work though.

--- plugins/gemma_fix.js
+++ plugins/gemma_fix.js
@@ -1,13 +1,8 @@
-export const FixGemmaUndefinedArgs = async () => {
+export const DefaultDescription = async ({ project, client, $, directory, worktree }) => {
   return {
     "tool.execute.before": async (input, output) => {
-      // Catch the broken 'task' tool calls
-      if (input.tool === "task" && !output.args.description) {
-        output.args.description = 'Delegated subtask (auto-recovered)';
-      }
-      // Catch the broken 'bash' tool calls (another common Gemma 4 casualty)
       if (input.tool === "bash" && !output.args.description) {
-        output.args.description = 'Execute a bash command (auto-recovered)';
+        output.args.description='Execute a command'
       }
     }
   }
